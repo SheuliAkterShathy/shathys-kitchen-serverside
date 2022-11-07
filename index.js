45a8d3f34239a154d8d8732eed037e42f3822406
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-11-08 04:17:31 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 // const jwt = require('jsonwebtoken');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -11,12 +11,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7czv3fq.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
 async function run(){
  try{
+    const serviceCollection = client.db('shathysKitchen').collection('services');
 
  }
  finally{
-    
+
  }
 }
 run().catch(err => console.error(err));
